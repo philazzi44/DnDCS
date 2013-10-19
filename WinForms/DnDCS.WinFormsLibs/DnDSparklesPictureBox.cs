9bfeccb3927b394135339bc95f7900d51caea2ed
Andrew Burgess <andrew@deceptacle.com>
2013-10-19 14:23:32 -0400

    Antialiasing of view area

--- WinForms/DnDCS.WinFormsLibs/DnDSparklesPictureBox.cs
+++ WinForms/DnDCS.WinFormsLibs/DnDSparklesPictureBox.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClipperLib;
 using DnDCS.Libs.SimpleObjects;
 using DnDCS.WinFormsLibs.Assets;
 
@@ -260,14 +263,14 @@ namespace DnDCS.WinFormsLibs
         protected override void OnPaint(PaintEventArgs e)
         {
             e.Graphics.Clear(BackColor);
-            //if (IsBlackoutOn)
-            //{
-            //    DrawBlackout(e.Graphics);
-            //}
-            //else
-            //{
+            if (IsBlackoutOn)
+            {
+                DrawBlackout(e.Graphics);
+            }
+            else
+            {
                 DrawImage(e.Graphics);
-            //}
+            }
             base.OnPaint(e);
         }
 
@@ -301,12 +304,18 @@ namespace DnDCS.WinFormsLibs
                 return;
             }
 
-            var boundingBoxBuffered = GetBoundingBox(points);
+            var polygon = new List<IntPoint>(points.Select(x => new IntPoint(x.X, x.Y)));
+            var polygons = new List<List<IntPoint>>() { polygon };
+            polygons = Clipper.OffsetPolygons(polygons, 48, JoinType.jtRound);
+
+            var offsetPoints = polygons[0].Select(x => new SimplePoint((int)x.X, (int)x.Y)).ToArray();
+
+            var boundingBoxBuffered = GetBoundingBox(offsetPoints, 4);
             var boundingBox = GetBoundingBox(points, 0);
 
             var bmd = map.LockBits(boundingBoxBuffered, ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
             var pixelSize = 4;
-            for (var y = 0; y < bmd.Height; y++)
+            Parallel.For(0, bmd.Height, (y) =>
             {
                 var row = (byte*)bmd.Scan0 + (y * bmd.Stride);
                 for (var x = 0; x < bmd.Width; x++)
@@ -314,8 +323,7 @@ namespace DnDCS.WinFormsLibs
                     var offsetX = x + boundingBoxBuffered.X;
                     var offsetY = y + boundingBoxBuffered.Y;
 
-                    if (offsetX < boundingBox.X || offsetX > boundingBox.X + boundingBox.Width ||
-                        offsetY < boundingBox.Y || offsetY > boundingBox.Y + boundingBox.Height)
+                    if (isClearing && row[x *pixelSize + 3] == 255)
                     {
                         continue;
                     }
@@ -324,8 +332,24 @@ namespace DnDCS.WinFormsLibs
                     {
                         row[x * pixelSize + 3] = (byte)(isClearing ? 255 : 0);
                     }
+                    else if (isClearing && IsPointInPolygon(offsetPoints, offsetX, offsetY))
+                    {
+                        var testPoint = new SimplePoint(offsetX, offsetY);
+                        var dist = LineToPointDistance2D(points[0], points[1], testPoint);
+                        for (int i = 0, j = points.Length - 1; i < points.Length; j = i++)
+                        {
+                            var newDist = LineToPointDistance2D(points[j], points[i], testPoint);
+                            if (newDist < dist)
+                                dist = newDist;
+                        }
+
+                        var alpha = (255 - 5.5 * dist);
+                        alpha = Math.Max(Math.Floor(alpha), 0);
+                        alpha = Math.Min(alpha + row[x * pixelSize + 3], 255);
+                        row[x * pixelSize + 3] = (byte)(alpha);
+                    }
                 }
-            }
+            });
 
             map.UnlockBits(bmd);
             Invalidate();
@@ -449,5 +473,69 @@ namespace DnDCS.WinFormsLibs
 
             CheckBounds();
         }
+
+        #region Maths
+        //Compute the dot product AB . AC
+        private double DotProduct(double[] pointA, double[] pointB, double[] pointC)
+        {
+            double[] AB = new double[2];
+            double[] BC = new double[2];
+            AB[0] = pointB[0] - pointA[0];
+            AB[1] = pointB[1] - pointA[1];
+            BC[0] = pointC[0] - pointB[0];
+            BC[1] = pointC[1] - pointB[1];
+            double dot = AB[0] * BC[0] + AB[1] * BC[1];
+
+            return dot;
+        }
+
+        //Compute the cross product AB x AC
+        private double CrossProduct(double[] pointA, double[] pointB, double[] pointC)
+        {
+            double[] AB = new double[2];
+            double[] AC = new double[2];
+            AB[0] = pointB[0] - pointA[0];
+            AB[1] = pointB[1] - pointA[1];
+            AC[0] = pointC[0] - pointA[0];
+            AC[1] = pointC[1] - pointA[1];
+            double cross = AB[0] * AC[1] - AB[1] * AC[0];
+
+            return cross;
+        }
+
+        //Compute the distance from A to B
+        private double Distance(double[] pointA, double[] pointB)
+        {
+            double d1 = pointA[0] - pointB[0];
+            double d2 = pointA[1] - pointB[1];
+
+            return Math.Sqrt(d1 * d1 + d2 * d2);
+        }
+
+        //Compute the distance from AB to C
+        //if isSegment is true, AB is a segment, not a line.
+        private double LineToPointDistance2D(SimplePoint linePoint1, SimplePoint linePoint2, SimplePoint pointTest, bool isSegment = true)
+        {
+            if (linePoint1.X == linePoint1.X && linePoint1.Y == linePoint2.Y)
+                return 255;
+
+            var pointA = new double[] { linePoint1.X, linePoint1.Y };
+            var pointB = new double[] { linePoint2.X, linePoint2.Y };
+            var pointC = new double[] { pointTest.X, pointTest.Y };
+            double dist = CrossProduct(pointA, pointB, pointC) / Distance(pointA, pointB);
+            if (isSegment)
+            {
+                double dot1 = DotProduct(pointA, pointB, pointC);
+                if (dot1 > 0)
+                    return Distance(pointB, pointC);
+
+                double dot2 = DotProduct(pointB, pointA, pointC);
+                if (dot2 > 0)
+                    return Distance(pointA, pointC);
+            }
+
+            return Math.Abs(dist);
+        } 
+        #endregion
     }
 }
