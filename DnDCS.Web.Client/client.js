fa755faa1f827fece5074ed72ef9688b90af706e
Phil Azzi <philazzi44@gmail.com>
2013-11-01 18:46:53 -0400

    Added folder for Web Client.

---
+++ DnDCS.Web.Client/client.js
@@ -0,0 +1,27 @@
+var ws;
+
+function connect() {
+	ws = new WebSocket("ws://localhost:11001/service");
+	ws.onopen = function () {
+		alert("Connection opened...");
+	};
+
+	ws.onmessage = function (evt) {
+		var received_msg = evt.data;
+		alert("Message Received: '" + received_msg + "'.");
+	};
+
+	ws.onclose = function () {
+		alert("Connection closed...");
+	};
+
+	ws.onerror = function (evt) {
+		alert("Connection errored... " + evt.data);
+	};
+};
+
+function sendInfo() {
+	var value = document.getElementById("sendText").value;
+	alert("Sending '" + value + "'...");
+	ws.send(value);
+}
